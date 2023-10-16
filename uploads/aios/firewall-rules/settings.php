d1cea354356a4a1eda6a045f2644a6a926720bee
栗田久海 <kurita_hisami@liginc.co.jp>
2023-10-16 17:33:54 +0900

    update: plugins

---
+++ uploads/aios/firewall-rules/settings.php
@@ -0,0 +1,6 @@
+<?php __halt_compiler();
+/**
+ * This file was created by All In One Security (AIOS) plugin.
+ * The file is required for storing and retrieving your firewall's settings.
+ */
+{"aios_enable_rename_login_page":"","aios_login_page_slug":"","aios_enable_brute_force_attack_prevention":"","aios_brute_force_secret_word":"","aios_cookie_based_brute_force_redirect_url":"http:\/\/127.0.0.1","aios_brute_force_attack_prevention_pw_protected_exception":"","aios_brute_force_attack_prevention_ajax_exception":"","aios_brute_force_secret_cookie_name":"aios_brute_force_secret_70490311fe7c84acda8886406a6d884b","aios_brute_force_cookie_salt":"fe49aef57ffccf96dce529e92fb68ddc06cd29a0ada5bf1dbbf42487ace995d7b542f1a11cb93edd","aiowps_blacklist_user_agents":[],"aios_ip_retrieve_method":0,"aiowps_ip_retrieve_method":"","aiowps_blacklist_ips":[]}
